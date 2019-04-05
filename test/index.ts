b250d319af807d027dcbfbea161d837d52b11a2d
yacut <roman.rogozhnikov@gmail.com>
2019-04-05 12:46:36 +0200

    add test setup

---
+++ test/index.ts
@@ -0,0 +1,10 @@
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+describe("getGoogleApiClient", () => {
+  it("should add some tests", () => {
+    expect(1).to.be.equal(1);
+  });
+});
